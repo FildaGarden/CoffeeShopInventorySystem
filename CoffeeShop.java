0aea814944d9e8fb49b3af43a3ac78384726fbaf
Filip Zahradník <66244844+FildaGarden@users.noreply.github.com>
2024-04-21 15:37:58 +0100

    Delete CoffeeShop.java

--- CoffeeShop.java
+++
@@ -1,7 +0,0 @@
-public class CoffeeShop {
-    //Main method that launches the app
-    public static void main(String[] args) {
-        CoffeeShopSystem cShopSystem = new CoffeeShopSystem();
-        cShopSystem.run();
-    }
-}
